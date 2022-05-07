9661787f6f37a612584e255c663beb9a543254f6
Imonuil-Suleimanov <imasuleymanov@gmail.com>
2022-05-07 16:15:35 +0600

    Player can enter their names

--- index.py
+++ index.py
@@ -6,3 +6,7 @@ print("|---------------------------------------------|")
 time.sleep(0.8)
 print('\nGame is loading ...\n')
 time.sleep(0.8)
+
+
+player_1= [input("Player 1 enter your name: "), "X"]
+player_2 = [input("Player 2 enter your name: "), "O"]
