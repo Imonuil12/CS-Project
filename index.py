6c8a19b995163e3ad422c95b94906044acdc36dc
Imonuil-Suleimanov <imasuleymanov@gmail.com>
2022-05-07 14:48:10 +0600

    first commit

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
-print("Hello world")
-
-print("Test 123 ...")
+import time
+print("|---------------------------------------------|")
+print("| Hello Friends, Welcome to TicTacToe Game!!! |")
+print("| -------------Let's Start!!!!--------------- |")
+print("|---------------------------------------------|")
+time.sleep(0.8)
+print('\nGame is loading ...\n')
+time.sleep(0.8)
