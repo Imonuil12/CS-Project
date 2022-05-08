8199b79c827424c7734ec7f57f5626a542442839
Imonuil-Suleimanov <imasuleymanov@gmail.com>
2022-05-08 23:38:41 +0600

    a

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from math import *
+from turtle import clear
 
 root= Tk()
 root.title("Calculator")
@@ -12,3 +13,13 @@ status= False
 text_input = Entry(root, width=50, relief=FLAT, justify=LEFT)
 text_input.grid(row=0, column=0, columnspan=6, ipady=100)
 text_input.focus_set()
+
+def get_expr(operand=""):
+    global status
+    if str(operand).isdigit() and status == True:
+        clear()
+        status = False
+    status = False
+    global expression
+    expression = text_input.get() + str(operand)
+    return expression
